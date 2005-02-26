1314d919a5992dad73c236e354e2fca2fca83c75
trans <trans@jabberwocky.ca>
2005-02-26 20:44:11 +0000

    Converted to use new typing system

--- src/sdrl/sdrl.c
+++ src/sdrl/sdrl.c
@@ -10,14 +10,14 @@
 
 #include "sdrl.h"
 #include "expr.h"
+#include "type.h"
 #include "parse.h"
 #include "value.h"
 #include "bindings.h"
 #include "types.h"
 
 
-static int sdrl_returnvalue(struct sdrl_machine *, char *);
-static char sdrl_getinput(struct sdrl_machine *, char *);
+static int sdrl_evaluate_funcptr(struct sdrl_machine *, int (*)(struct sdrl_machine *));
 
 /**
  * Create a machine for executing code
@@ -30,8 +30,14 @@ struct sdrl_machine *sdrl_create_machine(void)
 		return(NULL);
 	mach->ret = NULL;
 	mach->opr = NULL;
+	mach->type_env = sdrl_create_environment(0, NULL);
 	mach->env = sdrl_create_environment(0, NULL);
 	mach->code = NULL;
+
+	sdrl_bind_type(mach->type_env, "number", sdrl_make_type(0, 0, NULL, NULL));
+	sdrl_bind_type(mach->type_env, "string", sdrl_make_type(SDRL_VARIABLE_SIZE, 0, NULL, NULL));
+	sdrl_bind_type(mach->type_env, "expr", sdrl_make_type(0, 0, (sdrl_evaluate_t) sdrl_evaluate_expr, NULL));
+	sdrl_bind_type(mach->type_env, "funcptr", sdrl_make_type(0, 0, (sdrl_evaluate_t) sdrl_evaluate_funcptr, NULL));
 
 	return(mach);
 }
@@ -43,6 +49,7 @@ int sdrl_destroy_machine(struct sdrl_machine *mach)
 {
 	sdrl_destroy_expr(mach->code);
 	sdrl_destroy_environment(mach->env);
+	sdrl_destroy_environment(mach->type_env);
 	sdrl_destroy_value(mach->ret);
 	sdrl_destroy_value(mach->opr);
 	free(mach);
@@ -50,34 +57,38 @@ int sdrl_destroy_machine(struct sdrl_machine *mach)
 }
 
 /**
- * Evaluate recursively the sdrl_expr and either return 0 with the result
- * stored in sdrl_machine.ret or return an error code.
+ * Evaluate all the exprs in the list, expr.
  */
 int sdrl_evaluate_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
 {
-	int err;
+	int ret;
 	struct sdrl_expr *cur;
-	struct sdrl_value *args = NULL;
 
+	cur = expr;
+	while (cur) {
+		if (ret = sdrl_evaluate_single_expr(mach, cur))
+			return(ret);
+		cur = cur->next;
+	}
+	return(0);
+}
+
+/**
+ * Evaluate recursively the single sdrl_expr and either return 0 with the result
+ * stored in sdrl_machine.ret or return an error code.
+ */
+int sdrl_evaluate_single_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
+{
 	if (!expr)
 		return(0);
 	else if (expr->type == SDRL_ET_NUMBER)
-		mach->ret = sdrl_make_value(SDRL_VT_NUMBER, (sdrl_data_t) expr->data.number, 0, NULL);
+		mach->ret = sdrl_make_value(sdrl_find_type(mach->type_env, "number"), (sdrl_data_t) expr->data.number, 0, NULL);
 	else if (expr->type == SDRL_ET_NAME)
-		mach->ret = sdrl_make_value(SDRL_VT_STRING, (sdrl_data_t) expr->data.name, strlen(expr->data.name), NULL);
+		mach->ret = sdrl_make_value(sdrl_find_type(mach->type_env, "string"), (sdrl_data_t) expr->data.name, strlen(expr->data.name), NULL);
 	else if (expr->type == SDRL_ET_CALL) {
-		// TODO check to see if the function should be called using params or args
-		cur = expr->data.expr;
-		while (cur) {
-			if (err = sdrl_evaluate_expr(mach, cur))
-				return(err);
-			sdrl_push_value(&args, mach->ret);
-			cur = cur->next;
-		}
-		if (args->type != SDRL_VT_STRING)
+		if (expr->data.expr->type != SDRL_ET_NAME)
 			return(ERR_INVALID_FUNCTION);
-		if (err = sdrl_evaluate_function(mach, args->data.str, args->next))
-			return(err);
+		return(sdrl_evaluate_call(mach, expr->data.expr->data.name, expr->data.expr->next));
 	}
 	else
 		return(ERR_INVALID_AST_TYPE);
@@ -85,15 +96,17 @@ int sdrl_evaluate_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
 }
 
 /**
- * Lookup func (must be type expr) and evaluate as a function call
+ * Evaluate expression as a function call
  */
-int sdrl_evaluate_function(struct sdrl_machine *mach, char *func, struct sdrl_value *params)
+int sdrl_evaluate_call(struct sdrl_machine *mach, char *name, struct sdrl_expr *expr)
 {
 	int ret;
-	struct sdrl_value *bind;
+	struct sdrl_expr *cur;
+	struct sdrl_value *func;
+	struct sdrl_value *args = NULL;
 	struct sdrl_environment *parent;
 
-	if (!(bind = sdrl_find_binding(mach->env, func)))
+	if (!(func = sdrl_find_value(mach->env, name)))
 		return(ERR_NOT_FOUND);
 	parent = mach->env;
 	if (!(mach->env = sdrl_create_environment(0, parent))) {
@@ -101,16 +114,46 @@ int sdrl_evaluate_function(struct sdrl_machine *mach, char *func, struct sdrl_va
 		return(ERR_OUT_OF_MEMORY);
 	}
 
-	if (bind->type == SDRL_VT_EXPR)
-		ret = sdrl_evaluate_expr(mach, bind->data.expr);
-	else if (bind->type == SDRL_VT_FUNCPTR)
-		ret = ((int (*)(struct sdrl_machine *)) bind->data.str)(mach);
-	else
-		ret = ERR_INVALID_TYPE;
+	sdrl_destroy_value(mach->ret);
+	sdrl_destroy_value(mach->opr);
+	mach->ret = mach->opr = NULL;
+
+	if (func->type->evaluate) {
+		if (sdrl_type_eval_params_m(func->type)) {
+			cur = expr;
+			while (cur) {
+				sdrl_evaluate_single_expr(mach, expr);
+				sdrl_push_value(&args, mach->ret);
+				mach->ret = NULL;
+				cur = cur->next;
+			}
+			mach->opr = args;
+			func->type->evaluate(mach, func->data.ptr);
+		}
+		else {
+			mach->ret = sdrl_make_value(sdrl_find_type(mach->type_env, "expr"), (sdrl_data_t) (void *) expr, 0, NULL);
+			func->type->evaluate(mach, func->data.ptr);
+		}
+	}
+	else {
+		if (expr)
+			return(ERR_INVALID_PARAMS);
+		mach->ret = sdrl_duplicate_value(func);
+	}
 
 	sdrl_destroy_environment(mach->env);
 	mach->env = parent;
 	return(ret);
+}
+
+/*** Local Functions ***/
+
+/**
+ * Evaluate function for the funcptr type.
+ */
+static int sdrl_evaluate_funcptr(struct sdrl_machine *mach, int (*func)(struct sdrl_machine *))
+{
+	return(func(mach));
 }
 
 
--- src/sdrl/sdrl.h
+++ src/sdrl/sdrl.h
@@ -8,6 +8,8 @@
 #ifndef SDRL_H
 #define SDRL_H
 
+#include "expr.h"
+#include "type.h"
 #include "input.h"
 #include "value.h"
 #include "parse.h"
@@ -18,6 +20,7 @@
 struct sdrl_machine {
 	struct sdrl_value *ret;
 	struct sdrl_value *opr;
+	struct sdrl_environment *type_env;
 	struct sdrl_environment *env;
 	struct sdrl_expr *code;
 };
@@ -26,7 +29,8 @@ struct sdrl_machine *sdrl_create_machine(void);
 int sdrl_destroy_machine(struct sdrl_machine *);
 
 int sdrl_evaluate_expr(struct sdrl_machine *, struct sdrl_expr *);
-int sdrl_evaluate_function(struct sdrl_machine *, char *, struct sdrl_value *);
+int sdrl_evaluate_single_expr(struct sdrl_machine *, struct sdrl_expr *);
+int sdrl_evaluate_call(struct sdrl_machine *, char *, struct sdrl_expr *);
 
 #endif
 
