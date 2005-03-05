48d3ee3dd8dc9ef6672a485b94a83cd63c1dcbff
trans <trans@jabberwocky.ca>
2005-03-05 02:14:26 +0000

    Added function parameter support

--- src/sdrl/sdrl.c
+++ src/sdrl/sdrl.c
@@ -17,7 +17,9 @@
 #include "types.h"
 
 
-static int sdrl_evaluate_funcptr(struct sdrl_machine *, int (*)(struct sdrl_machine *));
+static int sdrl_evaluate_expr_type(struct sdrl_machine *, struct sdrl_expr *, struct sdrl_value *);
+static int sdrl_evaluate_form_type(struct sdrl_machine *, int (*)(struct sdrl_machine *, struct sdrl_expr *), struct sdrl_expr *);
+static int sdrl_evaluate_builtin_type(struct sdrl_machine *, int (*)(struct sdrl_machine *, struct sdrl_value *), struct sdrl_value *);
 
 /**
  * Create a machine for executing code
@@ -29,15 +31,15 @@ struct sdrl_machine *sdrl_create_machine(void)
 	if (!(mach = (struct sdrl_machine *) malloc(sizeof(struct sdrl_machine))))
 		return(NULL);
 	mach->ret = NULL;
-	mach->opr = NULL;
 	mach->type_env = sdrl_create_environment(0, NULL);
 	mach->env = sdrl_create_environment(0, NULL);
 	mach->code = NULL;
 
-	sdrl_bind_type(mach->type_env, "number", sdrl_make_type(0, 0, NULL, NULL));
-	sdrl_bind_type(mach->type_env, "string", sdrl_make_type(SDRL_VARIABLE_SIZE, 0, NULL, NULL));
-	sdrl_bind_type(mach->type_env, "expr", sdrl_make_type(0, 0, (sdrl_evaluate_t) sdrl_evaluate_expr, NULL));
-	sdrl_bind_type(mach->type_env, "funcptr", sdrl_make_type(0, 0, (sdrl_evaluate_t) sdrl_evaluate_funcptr, NULL));
+	sdrl_bind_type(mach->type_env, "number", sdrl_make_type(0, SDRL_BT_NUMBER, NULL, NULL));
+	sdrl_bind_type(mach->type_env, "string", sdrl_make_type(SDRL_VARIABLE_SIZE, SDRL_BT_STRING, NULL, NULL));
+	sdrl_bind_type(mach->type_env, "expr", sdrl_make_type(0, SDRL_BT_POINTER, (sdrl_evaluate_t) sdrl_evaluate_expr_type, NULL));
+	sdrl_bind_type(mach->type_env, "form", sdrl_make_type(0, SDRL_BT_POINTER | SDRL_TBF_PASS_EXPRS, (sdrl_evaluate_t) sdrl_evaluate_form_type, NULL));
+	sdrl_bind_type(mach->type_env, "builtin", sdrl_make_type(0, SDRL_BT_POINTER, (sdrl_evaluate_t) sdrl_evaluate_builtin_type, NULL));
 
 	return(mach);
 }
@@ -51,7 +53,6 @@ int sdrl_destroy_machine(struct sdrl_machine *mach)
 	sdrl_destroy_environment(mach->env);
 	sdrl_destroy_environment(mach->type_env);
 	sdrl_destroy_value(mach->ret);
-	sdrl_destroy_value(mach->opr);
 	free(mach);
 	return(0);
 }
@@ -59,14 +60,14 @@ int sdrl_destroy_machine(struct sdrl_machine *mach)
 /**
  * Evaluate all the exprs in the list, expr.
  */
-int sdrl_evaluate_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
+int sdrl_evaluate_expr_list(struct sdrl_machine *mach, struct sdrl_expr *expr)
 {
 	int ret;
 	struct sdrl_expr *cur;
 
 	cur = expr;
 	while (cur) {
-		if (ret = sdrl_evaluate_single_expr(mach, cur))
+		if (ret = sdrl_evaluate_expr(mach, cur))
 			return(ret);
 		cur = cur->next;
 	}
@@ -77,7 +78,7 @@ int sdrl_evaluate_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
  * Evaluate recursively the single sdrl_expr and either return 0 with the result
  * stored in sdrl_machine.ret or return an error code.
  */
-int sdrl_evaluate_single_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
+int sdrl_evaluate_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
 {
 	if (!expr)
 		return(0);
@@ -98,62 +99,94 @@ int sdrl_evaluate_single_expr(struct sdrl_machine *mach, struct sdrl_expr *expr)
 /**
  * Evaluate expression as a function call
  */
-int sdrl_evaluate_call(struct sdrl_machine *mach, char *name, struct sdrl_expr *expr)
+int sdrl_evaluate_call(struct sdrl_machine *mach, char *name, struct sdrl_expr *exprs)
 {
-	int ret;
-	struct sdrl_expr *cur;
-	struct sdrl_value *func;
-	struct sdrl_value *args = NULL;
-	struct sdrl_environment *parent;
+	int ret = 0;
+	struct sdrl_value *func, *args = NULL;
 
 	if (!(func = sdrl_find_value(mach->env, name)))
 		return(ERR_NOT_FOUND);
+
+	sdrl_destroy_value(mach->ret);
+	mach->ret = NULL;
+
+	if (func->type->evaluate) {
+		if (sdrl_type_pass_exprs_m(func->type))
+			ret = func->type->evaluate(mach, func->data.ptr, exprs);
+		else {
+			if (ret = sdrl_evaluate_params(mach, exprs, &args))
+				return(ret);
+			ret = func->type->evaluate(mach, func->data.ptr, args);
+		}
+	}
+	else {
+		if (exprs)
+			return(ERR_INVALID_PARAMS);
+		mach->ret = sdrl_duplicate_value(func);
+	}
+
+	return(ret);
+}
+
+/**
+ * Evaluate all the exprs and build a list of the corresponding return values.
+ */
+int sdrl_evaluate_params(struct sdrl_machine *mach, struct sdrl_expr *exprs, struct sdrl_value **args)
+{
+	int ret;
+	struct sdrl_expr *cur;
+
+	sdrl_destroy_value(mach->ret);
+	mach->ret = NULL;
+
+	cur = exprs;
+	while (cur) {
+		if (ret = sdrl_evaluate_expr(mach, cur))
+			return(ret);
+		sdrl_push_value(args, mach->ret);
+		mach->ret = NULL;
+		cur = cur->next;
+	}
+	return(0);
+}
+
+/*** Local Functions ***/
+
+/**
+ * Evaluate function for the cmdptr type.
+ */
+static int sdrl_evaluate_expr_type(struct sdrl_machine *mach, struct sdrl_expr *expr, struct sdrl_value *params)
+{
+	int ret = 0;
+	struct sdrl_environment *parent;
+
 	parent = mach->env;
 	if (!(mach->env = sdrl_create_environment(0, parent))) {
 		mach->env = parent;
 		return(ERR_OUT_OF_MEMORY);
 	}
 
-	sdrl_destroy_value(mach->ret);
-	sdrl_destroy_value(mach->opr);
-	mach->ret = mach->opr = NULL;
-
-	if (func->type->evaluate) {
-		if (sdrl_type_eval_params_m(func->type)) {
-			cur = expr;
-			while (cur) {
-				sdrl_evaluate_single_expr(mach, expr);
-				sdrl_push_value(&args, mach->ret);
-				mach->ret = NULL;
-				cur = cur->next;
-			}
-			mach->opr = args;
-			func->type->evaluate(mach, func->data.ptr);
-		}
-		else {
-			mach->ret = sdrl_make_value(sdrl_find_type(mach->type_env, "expr"), (sdrl_data_t) (void *) expr, 0, NULL);
-			func->type->evaluate(mach, func->data.ptr);
-		}
-	}
-	else {
-		if (expr)
-			return(ERR_INVALID_PARAMS);
-		mach->ret = sdrl_duplicate_value(func);
-	}
+	sdrl_bind_value(mach->env, "_", params);
+	ret = sdrl_evaluate_expr_list(mach, expr);
 
 	sdrl_destroy_environment(mach->env);
 	mach->env = parent;
 	return(ret);
 }
 
-/*** Local Functions ***/
-
 /**
- * Evaluate function for the funcptr type.
+ * Evaluate function for the form type.
  */
-static int sdrl_evaluate_funcptr(struct sdrl_machine *mach, int (*func)(struct sdrl_machine *))
+static int sdrl_evaluate_form_type(struct sdrl_machine *mach, int (*func)(struct sdrl_machine *, struct sdrl_expr *), struct sdrl_expr *params)
 {
-	return(func(mach));
+	return(func(mach, params));
 }
 
+/**
+ * Evaluate function for the builtin type.
+ */
+static int sdrl_evaluate_builtin_type(struct sdrl_machine *mach, int (*func)(struct sdrl_machine *, struct sdrl_value *), struct sdrl_value *args)
+{
+	return(func(mach, args));
+}
 
--- src/sdrl/sdrl.h
+++ src/sdrl/sdrl.h
@@ -19,7 +19,6 @@
 
 struct sdrl_machine {
 	struct sdrl_value *ret;
-	struct sdrl_value *opr;
 	struct sdrl_environment *type_env;
 	struct sdrl_environment *env;
 	struct sdrl_expr *code;
@@ -28,9 +27,10 @@ struct sdrl_machine {
 struct sdrl_machine *sdrl_create_machine(void);
 int sdrl_destroy_machine(struct sdrl_machine *);
 
+int sdrl_evaluate_expr_list(struct sdrl_machine *, struct sdrl_expr *);
 int sdrl_evaluate_expr(struct sdrl_machine *, struct sdrl_expr *);
-int sdrl_evaluate_single_expr(struct sdrl_machine *, struct sdrl_expr *);
 int sdrl_evaluate_call(struct sdrl_machine *, char *, struct sdrl_expr *);
+int sdrl_evaluate_params(struct sdrl_machine *, struct sdrl_expr *, struct sdrl_value **);
 
 #endif
 
