974ae5341c09d019f525b13431ed209ab534e26c
trans <trans@jabberwocky.ca>
2005-03-19 16:53:05 +0000

    Modified interpreter stub for new sdrl interface.

--- src/main.c
+++ src/main.c
@@ -7,39 +7,38 @@
 #include <stdio.h>
 
 #include <sdrl.h>
-#include "library.h"
 
 char *infile = NULL;
 
 int parse_cmdline(int, char **);
-int print_error(struct sdrl_machine *, int);
+int print_result(struct sdrl_machine *, int);
 
 main(int argc, char **argv)
 {
-	struct sdrl_machine *sdrl;
+	struct sdrl_machine *mach;
 
 	if (parse_cmdline(argc, argv)) {
 		printf("Usage: %s [infile]\n", argv[0]);
 		return(1);
 	}
 
-	if (!(sdrl = create_sdrl_machine(NULL)))
+	if (!(mach = sdrl_create_machine()))
 		return(-1);
-
-	if (library_initialize(sdrl)) {
-		destroy_sdrl_machine(sdrl);
-		return(-1);
-	}
-
-	if (sdrl_loadfile(sdrl, infile)) {
-		printf("Cannot open file, %s\n", infile);
-		destroy_sdrl_machine(sdrl);
+	if (prim_initialize(mach)) {
+		printf("Error initializing primatives\n");
+		sdrl_destroy_machine(mach);
 		return(-1);
 	}
 
-	print_error(sdrl, sdrl_run(sdrl));
+	if (!(mach->code = sdrl_parse_file(infile))) {
+		printf("Cannot parse file, %s\n", infile);
+		sdrl_destroy_machine(mach);
+		return(-1);
+	}
 
-	destroy_sdrl_machine(sdrl);
+	print_result(mach, sdrl_evaluate_expr_list(mach, mach->code));
+
+	sdrl_destroy_machine(mach);
 
 	return(0);
 }
@@ -64,47 +63,50 @@ int parse_cmdline(int argc, char **argv)
 		}
 	}
 
-	if (!(infile))
+	if (!infile)
 		return(-1);
 	return(0);
 
 }
 
-int print_error(struct sdrl_machine *mach, int error)
+int print_result(struct sdrl_machine *mach, int error)
 {
 	if (mach->ret) {
-		if (mach->ret->type & VT_STRING)
-			printf("%s\n", mach->ret->data.str);
-		else
-			printf("%d\n", mach->ret->data.num);
+//		printf("%f", mach->ret->data.number);
+//		printf("%s", mach->ret->data.str);
 	}
 
+	printf("\n");
 	switch (error) {
 		case 0:
+			printf("Exited Normally\n");
 			return(0);
-		case ERR_ENDOFINPUT:
+		case ERR_END_OF_INPUT:
 			printf("End Of Input Reached\n");
 			break;
-		case ERR_SYNTAXERROR:
+		case ERR_SYNTAX_ERROR:
 			printf("Syntax Error\n");
 			break;
-		case ERR_INVALIDPARAMS:
+		case ERR_INVALID_PARAMS:
 			printf("Invalid Parameters\n");
 			break;
-		case ERR_INVALIDTYPE:
+		case ERR_INVALID_TYPE:
 			printf("Invalid Type\n");
 			break;
-		case ERR_OUTOFMEMORY:
+		case ERR_OUT_OF_MEMORY:
 			printf("Out Of Memory\n");
 			break;
-		case ERR_OUTOFBOUNDS:
+		case ERR_OUT_OF_BOUNDS:
 			printf("Out Of Bounds\n");
 			break;
-		case ERR_NOTFOUND:
+		case ERR_NOT_FOUND:
 			printf("Binding Not Found\n");
 			break;
+		case ERR_PARAMS_ERROR:
+			printf("Error Evaluating Parameters\n");
+			break;
 		default:
-			printf("Unknown Error\n");
+			printf("Error Number: %d\n", error);
 			break;
 	}
 	return(-1);
