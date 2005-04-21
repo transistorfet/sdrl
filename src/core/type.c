2218e14752c82d00a88c394d37e10db314f8a103
trans <trans@jabberwocky.ca>
2005-04-21 20:25:57 +0000

    Added create function.

--- src/core/type.c
+++ src/core/type.c
@@ -14,7 +14,7 @@
 /**
  * Make a new generic sdrl type.
  */
-struct sdrl_type *sdrl_make_type(struct sdrl_heap *heap, int size, int bitflags, sdrl_evaluate_t evaluate, sdrl_duplicate_t duplicate, sdrl_destroy_t destroy)
+struct sdrl_type *sdrl_make_type(struct sdrl_heap *heap, int size, int bitflags, sdrl_create_t create, sdrl_evaluate_t evaluate, sdrl_duplicate_t duplicate, sdrl_destroy_t destroy)
 {
 	struct sdrl_type *type;
 
@@ -23,6 +23,7 @@ struct sdrl_type *sdrl_make_type(struct sdrl_heap *heap, int size, int bitflags,
 
 	type->size = size;
 	type->bitflags = bitflags;
+	type->create = create;
 	type->evaluate = evaluate;
 	type->duplicate = duplicate;
 	type->destroy = destroy;
