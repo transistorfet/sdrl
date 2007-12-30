4fa69e4e8edcfca2a5f605a35e0156f2eec45c16
transistor <trans@lorax.jabberwocky.ca>
2007-12-29 21:05:25 -0800

    Fixed so that it's compilable and generally accurate.

--- src/lib/io/types/file.c
+++ src/lib/io/types/file.c
@@ -15,7 +15,7 @@ static int sdrl_destroy_file_type(struct sdrl_heap *, void *);
 
 int sdrl_base_load_file_type(struct sdrl_machine *mach, const char *name)
 {
-	if (!(file_type = sdrl_make_type(mach->heap, 0, SDRL_BT_POINTER, (sdrl_create_t) sdrl_create_env_type, NULL, NULL, (sdrl_destroy_t) sdrl_destroy_env_type)))
+	if (!(file_type = sdrl_make_type(mach->heap, 0, SDRL_BT_POINTER, (sdrl_create_t) sdrl_create_file_type, NULL, NULL, (sdrl_destroy_t) sdrl_destroy_file_type)))
 		return(-1);
 	return(sdrl_add_binding(mach->type_env, name, file_type));
 }
@@ -29,12 +29,16 @@ static void *sdrl_create_file_type(struct sdrl_machine *mach, struct sdrl_value 
 {
 	FILE *fptr;
 	int ret = 0;
+	struct sdrl_type *string;
 
-	if (value) {
-		if (!(fopen(value->data.str, "r")))
-			ret = -1
-	}
-	sdrl_destroy_value(mach->heap, value);
+	if (!value)
+		ret = SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_INVALID_ARGS, NULL);
+	else if (!(string = sdrl_find_binding(mach->type_env, "string")))
+		ret = SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_NOT_FOUND, NULL);
+	else if (value->type != string)
+		ret = SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_INVALID_TYPE, NULL);
+	else if (!(fptr = fopen(value->data.str, "r")))
+		ret = SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_FAILED, "Error opening file");
 	if (ret)
 		return(NULL);
 	return((void *) fptr);
