0f033dfe7c7df92449deea83fa597348f1af7e7e
transistor <trans@lorax.jabberwocky.ca>
2007-12-30 21:42:33 -0800

    Converted the char function to use the utils functions as a test.

--- src/lib/string/funcs/char.c
+++ src/lib/string/funcs/char.c
@@ -17,17 +17,18 @@
 int sdrl_string_char(struct sdrl_machine *mach, struct sdrl_value *value)
 {
 	number_t ret = 0;
-	struct sdrl_type *number, *string;
+	struct sdrl_value *str, *index;
+	struct sdrl_type *num_type, *str_type;
 
-	if (!(value && value->next))
-		return(SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_INVALID_ARGS, NULL));
-	if (!(number = sdrl_find_binding(mach->type_env, "number")) || !(string = sdrl_find_binding(mach->type_env, "string")))
+	if (!(num_type = sdrl_find_binding(mach->type_env, "number"))
+	    || !(str_type = sdrl_find_binding(mach->type_env, "string")))
 		return(SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_NOT_FOUND, NULL));
-	if ((value->type != string) || (value->next->type != number))
-		return(SDRL_ERROR(mach, SDRL_ES_HIGH, SDRL_ERR_INVALID_TYPE, NULL));
-	if ((value->next->data.num >= 0) && (value->next->data.num < strlen(value->data.str)))
-		ret = value->data.str[ (int) value->next->data.num ];
-	mach->ret = sdrl_make_value(mach->heap, number, (sdrl_data_t) ret, 0, NULL);
+	if (!(str = sdrl_next_arg_check(mach, &value, str_type))
+	    || !(index = sdrl_next_arg_check(mach, &value, num_type)))
+		return(mach->error->err);
+	if ((index->data.num >= 0) && (index->data.num < strlen(str->data.str)))
+		ret = str->data.str[ (int) index->data.num ];
+	mach->ret = sdrl_make_value(mach->heap, num_type, (sdrl_data_t) ret, 0, NULL);
 	return(0);
 }
 
